7c05cec29866eac319459bd9371533cccf6ade46
pablo.swp <pablo.swp@d6563453-b5d7-442c-e6ea-2272614c8cdb>
2011-09-23 01:33:11 +0000

    Las clases de support y datatypes ya no son persistentes.

--- open-ehr-gen/grails-app/domain/hce/core/support/identification/PartyRef.groovy
+++
@@ -1,9 +0,0 @@
-package hce.core.support.identification
-
-class PartyRef extends ObjectRef {
-
-    public String toString()
-    {
-        return this.getClass().getSimpleName()+'-> '+this.namespace+' '+this.type+' '+this.objectId.toString()
-    }
-}
