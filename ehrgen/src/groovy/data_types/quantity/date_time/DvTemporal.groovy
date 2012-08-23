dfe4bbc090cae5928f6fb807d295028f07ef39e9
pablo.swp <pablo.swp@d6563453-b5d7-442c-e6ea-2272614c8cdb>
2012-08-23 20:54:24 +0000

    Agregado de la verficacion de permisos en menues.

---
+++ ehrgen/src/groovy/data_types/quantity/date_time/DvTemporal.groovy
@@ -0,0 +1,17 @@
+package data_types.quantity.date_time
+
+import data_types.basic.DataValue
+
+class DvTemporal extends DataValue {
+
+    String value
+
+    /*
+    static mapping = {
+        value column: "dvtemporal_value"
+    }
+
+    static constraints = {
+    }
+    */
+}
