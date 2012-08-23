21e878bf7ed42828b25b83fd1b35b7325e63dd7a
pablo.swp <pablo.swp@d6563453-b5d7-442c-e6ea-2272614c8cdb>
2012-08-23 20:54:24 +0000

    Agregado de la verficacion de permisos en menues.

---
+++ trunk/ehrgen/src/groovy/support/identification/PartyRef.groovy
@@ -0,0 +1,11 @@
+package support.identification
+
+import support.identification.ObjectRef
+
+class PartyRef extends ObjectRef {
+
+    public String toString()
+    {
+        return this.getClass().getSimpleName()+'-> '+this.namespace+' '+this.type+' '+this.objectId.toString()
+    }
+}
---
+++ trunk/ehrgen/src/groovy/support/identification/TemplateID.groovy
@@ -0,0 +1,13 @@
+package support.identification
+
+import support.identification.ObjectID
+
+/**
+ * Identificador para Templates. Lexical form to be determined.
+ *
+ * @author Leandro Carrasco
+ * @version 1.0
+ */
+class TemplateID extends ObjectID {
+
+}
