982a287597237e1b4870e3e7c07d076404ac7125
pablo.swp <pablo.swp@gmail.com>
2012-12-22 03:27:20 +0000

    Nueva estructura de domain y workflow

--- ehrgen/grails-app/domain/authorization/DomainPermit.groovy
+++ ehrgen/grails-app/domain/authorization/DomainPermit.groovy
@@ -1,5 +1,6 @@
 package authorization
 
+import domain.Domain
 import org.springframework.beans.BeanWrapper
 import org.springframework.beans.PropertyAccessorFactory
 import org.codehaus.groovy.grails.commons.ApplicationHolder
@@ -10,11 +11,11 @@ import org.codehaus.groovy.grails.commons.ApplicationHolder
  * Tener acceso significa poder listar, ver, crear, editar registros, a no ser que
  * Permit diga lo contrario, por ejemplo que solo deje listar y ver, y no crear o editar.
  * 
- * @author pab
+ * @author Pablo Pazos Gutierrez <pablo.pazos@cabolabs.com>
  */
 class DomainPermit {
     
-   String domain
+   long domain // id del Domain en la db
    String templateId = "*" // Si no se indica lo contrario, se tiene acceso a todos los templates
     
    static constraints = {
@@ -24,13 +25,14 @@ class DomainPermit {
     
    static void createDefault()
    {
-      def domains = ApplicationHolder.application.config.domains
+      //def domains = ApplicationHolder.application.config.domains
       def sections
       def templates
       def p
+	   def domains = Domain.list() // Los dominios se deben haber guardado previamente desde el Boostrap
       for (def domain in domains)
       {
-         sections = ApplicationHolder.application.config.templates2."${domain}"
+         sections = ApplicationHolder.application.config.templates2."${domain.name}"
          for (def section in sections)
          {
             //println section // ADMISION = ['prehospitalario.v1', 'contexto_del_evento.v1']
@@ -40,14 +42,14 @@ class DomainPermit {
             for (def template in templates)
             {
                p = new DomainPermit(
-                             domain: domain,
+                             domain: domain.id,
                              templateId: section.key+'-'+template) // ADMISION-prehospitalario.v1
                p.save()
             }
             
          }
          
-         p = new DomainPermit(domain: domain, templateId: '*')
+         p = new DomainPermit(domain: domain.id)
          p.save()
       }
    }
