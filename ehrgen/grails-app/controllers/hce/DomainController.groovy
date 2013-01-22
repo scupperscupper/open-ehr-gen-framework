ca6e058663265b7314dddb25a43dd3ed9de0a252
pablo.swp <pablo.swp@gmail.com>
2013-01-22 05:57:52 +0000

    Se agrega la creacion de admisiones.

--- ehrgen/grails-app/controllers/hce/DomainController.groovy
+++ ehrgen/grails-app/controllers/hce/DomainController.groovy
@@ -1,4 +1,33 @@
-package hce;
+/*
+Copyright 2013 CaboLabs.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This software was developed by Pablo Pazos at CaboLabs.com
+
+This software uses the openEHR Java Ref Impl developed by Rong Chen
+http://www.openehr.org/wiki/display/projects/Java+Project+Download
+
+This software uses MySQL Connector for Java developed by Oracle
+http://dev.mysql.com/downloads/connector/j/
+
+This software uses PostgreSQL JDBC Connector developed by Posrgresql.org
+http://jdbc.postgresql.org/
+
+This software uses XStream library developed by Jörg Schaible
+http://xstream.codehaus.org/
+*/
+package hce
 
 //import hce.core.common.directory.Folder
 import domain.Domain
@@ -130,6 +159,9 @@ class DomainController {
    }
    
    
+   // =======================================================================================
+   // ==== OPERACIONES SOBRE ADMISIONES (buenos candidatos para tener su propio controller)
+   //
    /**
     * Lista admisiones para el medico logueado en la vista de seleccion de dominios.
     */
@@ -148,6 +180,47 @@ class DomainController {
      render(template:'admissionList', model:[admissions:admissions])
    }
    
+   def admissionCreate = {
+   
+      def rvs = RoleValidity.withCriteria {
+        role {
+          eq('type', Role.MEDICO)
+        }
+      }
+      def physicians = rvs.performer
+      
+      rvs = RoleValidity.withCriteria {
+        role {
+          eq('type', Role.PACIENTE)
+        }
+      }
+      def patients = rvs.performer
+      
+      def domains = Domain.list()
+   
+      if (params.doit)
+      {
+         def admission = new Admission(
+            patientId: params.patientId,
+            physicianId: params.physicianId,
+            domainId: params.domainId
+         )
+         
+         if (!admission.save())
+         {
+            println admission.errors
+         }
+         
+         flash.message = "Admision creada con exito"
+         redirect(action:'list')
+         return
+      }
+   
+      return render(template:'admissionCreate', model:[physicians:physicians, patients:patients, domains:domains])
+   }
+   //
+   // ==== /OPERACIONES SOBRE ADMISIONES (buenos candidatos para tener su propio controller)
+   // =======================================================================================
    
    /**
     * Crea un nuevo dominio desde la UI.
