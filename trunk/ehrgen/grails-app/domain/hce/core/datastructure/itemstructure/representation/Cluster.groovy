975ba826f3d5de73f7df86af5e16c203052b51b3
pablo.swp@gmail.com <pablo.swp@gmail.com@d6563453-b5d7-442c-e6ea-2272614c8cdb>
2013-02-01 00:03:55 +0000

    Borro comentarios

--- trunk/ehrgen/grails-app/domain/hce/core/datastructure/itemstructure/representation/Cluster.groovy
+++ trunk/ehrgen/grails-app/domain/hce/core/datastructure/itemstructure/representation/Cluster.groovy
@@ -1,3 +1,32 @@
+/*
+Copyright 2013 CaboLabs.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This software was developed by Pablo Pazos at CaboLabs.com
+
+This software uses the openEHR Java Ref Impl developed by Rong Chen
+http://www.openehr.org/wiki/display/projects/Java+Project+Download
+
+This software uses MySQL Connector for Java developed by Oracle
+http://dev.mysql.com/downloads/connector/j/
+
+This software uses PostgreSQL JDBC Connector developed by Posrgresql.org
+http://jdbc.postgresql.org/
+
+This software uses XStream library developed by Jörg Schaible
+http://xstream.codehaus.org/
+*/
 package hce.core.datastructure.itemstructure.representation
 
 import hce.core.common.archetyped.Pathable
@@ -12,23 +41,4 @@ class Cluster extends Item {
         items column: "cluster_items"
         items cascade: "save-update"
     }
-    
-   /*
-   // rmParentId definido en Pathable
-   // Si no se pone tira except property [rmParent] not found on entity
-   static transients = ['padre']
-   Pathable getPadre()
-   {
-      if (!this.rmParentId) return null
-      return Pathable.get(this.rmParentId)
-   }
-   */
-   /*
-   void setRmParent(Pathable parent)
-   {
-      if (!parent) throw new Exception("parent no puede ser nulo")
-      if (!parent.id) throw new Exception("parent debe tener id (debe guardarse previamente en la base)")
-      this.rmParentId = parent.id
-   }
-   */
 }
