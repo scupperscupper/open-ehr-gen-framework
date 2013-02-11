effdddab5994a9d144e2e03e90fb1959098323a4
pablo.swp <pablo.swp@gmail.com>
2013-02-11 17:15:09 +0000

    Agrego comentarios para simplificar la configuracion

--- ehrgen/grails-app/conf/DataSource.groovy
+++ ehrgen/grails-app/conf/DataSource.groovy
@@ -14,45 +14,77 @@ hibernate {
 environments {
    development {
       dataSource {
-//          dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-//            url = "jdbc:hsqldb:mem:devDB"
          
-           pooling = true
-           
-		   driverClassName = "com.mysql.jdbc.Driver"
-           url = "jdbc:mysql://localhost:3306/oehr_dev?useUnicode=true&characterEncoding=UTF-8&characterSetResults=UTF-8"
-		   username = "root"
-           password = "vertrigo"
-		   
-		   /*
-		   driverClassName = "org.postgresql.Driver"
-		   url = "jdbc:postgresql://localhost:5432/ehrgen"
-		   username = "postgres"
-           password = "postgres"
-		   dialect = net.sf.hibernate.dialect.PostgreSQLDialect // the difference is.
-		   */
-           dbCreate = "create-drop"
-           //dbCreate = "update"
-           //loggingSql = true
+         // ====================================================================
+         // Conexiones
+         // ====================================================================
+         
+         // Configuracion para utilizar HSQL en memoria
+         // Selecciona la base de datos 'devDB'
+         // dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+         // url = "jdbc:hsqldb:mem:devDB"
+         
+         
+         // Configuracion para utilizar MySQL
+         // Selecciona la base de datos oehr_dev
+         driverClassName = "com.mysql.jdbc.Driver"
+         url = "jdbc:mysql://localhost:3306/oehr_dev?useUnicode=true&characterEncoding=UTF-8&characterSetResults=UTF-8"
+         username = "root"
+         password = "vertrigo"
+         
+         
+         // Configuracion para utilizar PostgreSQL
+         // driverClassName = "org.postgresql.Driver"
+         // url = "jdbc:postgresql://localhost:5432/ehrgen"
+         // username = "postgres"
+         // password = "postgres"
+         // dialect = net.sf.hibernate.dialect.PostgreSQLDialect
+         
+         // ====================================================================
+         // Otras configuraciones
+         // ====================================================================
+         
+         // Reutilizar conecciones existentes con la base de datos 
+         pooled = true
+         
+         // create-drop borra la base de datos y la genera de nuevo cada vez que se ejecuta la aplicacion
+         // Ver mas opciones en la seccion 3.3: http://grails.org/doc/1.0/guide/3.%20Configuration.html
+         dbCreate = "create-drop"
+         
+         // Poner en true para loguear consultas SQL
+         loggingSql = false
       }
    }
    test {
       dataSource {
-         dbCreate = "update"
+         
+         // Configuracion para utilizar HSQL en memoria
          url = "jdbc:hsqldb:mem:testDb"
+         
+         dbCreate = "update"
       }
    }
    production {
       dataSource {
-         //dbCreate = "update"
+         
+         // Configuracion para utilizar HSQL en archivo
+         // Selecciona la base de datos prodDb
          //url = "jdbc:hsqldb:file:prodDb;shutdown=true"
-            
-            pooling = true
-            driverClassName = "com.mysql.jdbc.Driver"
-            url = "jdbc:mysql://localhost:3306/oehr_dev?useUnicode=true&characterEncoding=UTF-8&characterSetResults=UTF-8"
-            dbCreate = "create-drop"
-            username = "root"
-            password = "vertrigo"
+         
+         // Configuracion para utilizar MySQL
+         // Selecciona la base de datos oehr_dev (por simplicidad "prod" utiliza la misma base que en "dev")
+         driverClassName = "com.mysql.jdbc.Driver"
+         url = "jdbc:mysql://localhost:3306/oehr_dev?useUnicode=true&characterEncoding=UTF-8&characterSetResults=UTF-8"
+         dbCreate = "update"
+         username = "root"
+         password = "vertrigo"
+         
+         // Reutilizar conecciones existentes con la base de datos
+         pooled = true
+         
+         // update utiliza la base de datos existente actualizando sus datos en el bootstrap cada vez que se ejecuta la aplicacion
+         // Ver mas opciones en la seccion 3.3: http://grails.org/doc/1.0/guide/3.%20Configuration.html
+         dbCreate = "update"
       }
    }
 }
